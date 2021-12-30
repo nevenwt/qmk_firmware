2c34c2d557ddfb3c1496b62f4555fe64a0d34427
Neven Wang-Tomic <nevenwt@gmail.com>
2021-12-30 12:02:58 -0800

    Update default keymap with RGB underglow change on secondary layer

--- keyboards/dztech/dz65rgb/keymaps/nevenwt/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/nevenwt/keymap.c
@@ -1,14 +1,19 @@
 #include QMK_KEYBOARD_H
 
+enum {
+  _QWERTY = 0,
+  _SECONDARY = 1
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_65_ansi(
+    [_QWERTY] = LAYOUT_65_ansi(
         KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_MPLY,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_VOLU,
         CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_VOLD,
         KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_DEL,
         KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    [1] = LAYOUT_65_ansi(
+    [_SECONDARY] = LAYOUT_65_ansi(
         KC_GESC,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
         _______,        RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,   KC_MNXT,
         CTL_T(KC_CAPS), RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST, KC_MPRV,
@@ -17,11 +22,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+  switch (get_highest_layer(state)) {
+    case _SECONDARY:
+      rgblight_sethsv(HSV_AZURE);
+      break;
+    default:
+      rgblight_sethsv(HSV_GOLD);
+      break;
+  }
+
+  return state;
+}
+
 void led_set_user(uint8_t usb_led) {
-  if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-    rgblight_mode_noeeprom(1);
-    rgblight_sethsv(HSV_RED);
-  } else {
-    rgblight_sethsv(HSV_GOLD);
+  // If on QWERTY layer
+  if (get_highest_layer(layer_state) == _QWERTY) {
+    // If caps lock active
+    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+      rgblight_mode_noeeprom(1);
+      rgblight_sethsv(HSV_RED);
+    // If caps lock not active
+    } else {
+      rgblight_sethsv(HSV_GOLD);
+    }
   }
 } 
